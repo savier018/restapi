7d1afbb72fb113a2670fd7b094c60364a18ccd5d
savier018 <acosta.savier.03@gmail.com>
2024-07-15 09:44:21 -0500

    haciendo la guia 17

--- swagger.js
+++ swagger.js
@@ -5,7 +5,10 @@ const doc = {
     "title": 'REST API',
     "description": 'REST API with Express and Firestore'
   },
-  "host": 'localhost:5500',
+  "host": 'restapi-0z6c.onrender.com',
+  "schemes": [
+     "https"
+   ],
   "basePath": "/api",
 };
 
