7c41dd2c5b143bb0bc63fe0290dc2ced552bac83
savier018 <acosta.savier.03@gmail.com>
2024-07-15 09:22:07 -0500

    subiendo la guia 16

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,22 @@ const admin = require('firebase-admin');
 const db = admin.firestore();
 
 exports.createItem = async (req, res) => {
+  /* 
+     #swagger.tags = ['Items']
+     #swagger.description = 'Create an item'
+     #swagger.summary = 'Create an item'
+     #swagger.parameters['data'] = {
+         in: 'body',
+         description: 'Data to create an item',
+         required: true,
+     }
+     #swagger.responses[201] = {
+         description: 'Item successfully created',
+     }
+     #swagger.responses[400] = {
+         description: 'Bad request',
+     }
+  */
   try {
     const data = req.body;
     const itemRef = await db.collection('items').add(data);
@@ -12,6 +28,24 @@ exports.createItem = async (req, res) => {
 };
 
 exports.getItem = async (req, res) => { 
+    /* 
+     #swagger.tags = ['Items']
+     #swagger.description = 'Get an item entry'
+     #swagger.summary = 'Get an item entry'
+     #swagger.parameters['id'] = {
+         description: 'Item id',
+         required: true,
+     }
+     #swagger.responses[404] = {
+         description: 'Item not found',
+     }
+     #swagger.responses[400] = {
+         description: 'Bad request',
+     }
+     #swagger.responses[200] = {
+         description: 'Get an item by id',
+     }
+   */
     try {
         const dataID = req.params.id;
         const itemRef = await db.collection('items').doc(dataID).get();
@@ -49,6 +83,17 @@ exports.deleteItem = async (req, res) => {
 }
 
 exports.getAllItems = async (req, res) => {
+  /* 
+     #swagger.tags = ['Items']
+     #swagger.description = 'Get all items entries'
+     #swagger.summary = 'Get all items entries'
+     #swagger.responses[200] = {
+         description: 'Items entries successfully obtained',
+     }
+     #swagger.responses[400] = {
+         description: 'Bad request',
+     }
+  */
   try {
     const itemsSnapshot = await db.collection('items').get();
     const items = [];
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_API)
 
+/* Referencia al módulo swagger-ui-express */
+const swaggerUi = require('swagger-ui-express')
+
+/* Referencia al archivo con la descripción */
+const swaggerFile = require('./swagger_output.json')
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -13,6 +19,9 @@ const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
+
+/* Ruta Base -> Documentación */
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/api', require('./routes/api'));
 
